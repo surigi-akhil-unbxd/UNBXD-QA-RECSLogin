da34408eac32950b485b1d920aaab3e6ca8bb35f
unbxd <unbxd@akhilsurigi.local>
2025-07-16 17:49:11 +0530

    Always use RemoteWebDriver when hubUrl is set; never use local drivers on CI

--- src/main/java/lib/BrowserInitializer.java
+++ src/main/java/lib/BrowserInitializer.java
@@ -93,17 +93,18 @@ public class BrowserInitializer {
                 System.out.println("[DEBUG] Using Selenium Grid at: " + hubUrl);
                 driver = new RemoteWebDriver(new URL(hubUrl), capabilities);
             } else if (browser == Browser.CHROME) {
-            String chromePath = getChromeDriverPath();
-            System.out.println("[DEBUG] Using ChromeDriver path: " + chromePath);
+                String chromePath = getChromeDriverPath();
+                System.out.println("[DEBUG] Using ChromeDriver path: " + chromePath);
+                System.setProperty(CHROME_DRIVER_EXE_PROPERTY, chromePath);
                 driver = new ChromeDriver(capabilities);
             } else if (browser == Browser.FIREFOX) {
-            System.setProperty("webdriver.gecko.driver", geckoDriverPath);
-            capabilities.setCapability("marionette", true);
-            System.out.println("[DEBUG] Using GeckoDriver path: " + geckoDriverPath);
+                System.setProperty("webdriver.gecko.driver", geckoDriverPath);
+                capabilities.setCapability("marionette", true);
+                System.out.println("[DEBUG] Using GeckoDriver path: " + geckoDriverPath);
                 driver = new FirefoxDriver(capabilities);
             } else {
                 System.out.println("[DEBUG] No matching browser found, returning null driver.");
-        }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
